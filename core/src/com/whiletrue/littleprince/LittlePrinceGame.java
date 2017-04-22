e75270fc8f9fd5a8ec7ac095658a593189171bab
Boris Tolstukha <boris.tolstukha@gmail.com>
2017-04-22 14:39:32 +0600

    planetOutline + cut

--- core/src/com/whiletrue/littleprince/LittlePrinceGame.java
+++ core/src/com/whiletrue/littleprince/LittlePrinceGame.java
@@ -4,30 +4,43 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.*;
 
 public class LittlePrinceGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	PolygonSpriteBatch batch;
+	Texture texture;
+	PolygonRegion region;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		texture = new Texture("planet.jpg");
+		TextureRegion textureRegion = new TextureRegion(texture);
+
+		float radius = 0.5f*textureRegion.getRegionWidth();
+
+		PlanetOutline planetOutline = new PlanetOutline(1);
+
+		planetOutline.cutLine(1f,0.5f);
+
+		float[] verticies = planetOutline.getVertices(radius,radius,radius);
+
+		region = new PolygonRegion(textureRegion, verticies, PlanetOutline.TRIANGLES);
+		batch = new PolygonSpriteBatch();
+		//debugRenderer = new PolygonRegionDebugRenderer();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(region, 128, 128, 256, 256);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		texture.dispose();
 	}
 }
---
+++ core/src/com/whiletrue/littleprince/PlanetOutline.java
@@ -0,0 +1,88 @@
+package com.whiletrue.littleprince;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
+import org.ejml.simple.SimpleMatrix;
+
+/**
+ * Created by boris_0mrym3f on 22.04.2017.
+ */
+public class PlanetOutline {
+
+    private final static short SEGMENT_COUNT = 30;
+    public final static short[] TRIANGLES = new short[SEGMENT_COUNT*3];
+
+    static {
+        for (short i=0; i<SEGMENT_COUNT; i++){
+            TRIANGLES[i*3] = 0;
+            TRIANGLES[i*3+1] = (short)(i+1);
+            TRIANGLES[i*3+2] = (short)(i+2);
+        }
+        TRIANGLES[SEGMENT_COUNT*3-1] = (short)1;
+    }
+
+    private float[] radiusValues = new float[SEGMENT_COUNT];
+
+
+    public PlanetOutline(float initialRadius){
+        for (int i=0; i<SEGMENT_COUNT; i++){
+            radiusValues[i] = initialRadius;
+        }
+    }
+
+    public float radius(float angle){
+        int segmentNumber = (int)(Math.round((angle/(2*Math.PI))*SEGMENT_COUNT)%SEGMENT_COUNT);
+        return radiusValues[segmentNumber];
+    }
+
+    float angle(int index){
+        return (float)((Math.PI*2)/SEGMENT_COUNT)*index;
+    }
+
+    float getX(float angle, float radius){
+        return (float) (radius*Math.cos(angle));
+    }
+    float getY(float angle, float radius){
+        return (float) (radius*Math.sin(angle));
+    }
+
+    public float[] getVertices(float originX, float originY, float scale){
+        float[] vertices = new float[(SEGMENT_COUNT+1)*2];
+        vertices[0] = originX;
+        vertices[1] = originY;
+        for(int i=0; i<SEGMENT_COUNT; i++){
+            float angle = angle(i);
+            vertices[(i+1)*2] = originX + scale * getX(angle,radiusValues[i]);
+            vertices[(i+1)*2+1] = originY + scale * getY(angle,radiusValues[i]);
+        }
+        return vertices;
+    }
+
+    public void cutLine(float k, float b){
+        SimpleMatrix A = new SimpleMatrix(2,2);
+        A.set(0, 0, k);
+        A.set(0, 1, -1);
+        A.set(1, 1, -1);
+        SimpleMatrix right = new SimpleMatrix(2,1);
+        right.set(0, 0, -b);
+        right.set(1, 0, 0);
+        for (int i=0; i<SEGMENT_COUNT; i++){
+            float angle = angle(i);
+            A.set(1, 0, (Math.tan(angle)));
+            SimpleMatrix rVect = A.solve(right);
+            double x = rVect.get(0,0);
+            double y = rVect.get(1,0);
+            double scalarProduct = x * Math.cos(angle) + y * Math.sin(angle);
+            double newRadius = Math.sqrt(x*x+y*y);
+            if(scalarProduct>0 && newRadius < radiusValues[i]){
+                radiusValues[i] = (float)newRadius;
+            }
+        }
+    }
+
+    /*public Polygon getPolygon(){
+        return new Polygon(getVertices());
+    }*/
+
+}
