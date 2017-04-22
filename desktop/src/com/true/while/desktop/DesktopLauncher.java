e8ee23dde0900086fabd39c8c737df4b68d291d8
Boris Tolstukha <boris.tolstukha@gmail.com>
2017-04-22 11:51:22 +0600

    initial commit

---
+++ desktop/src/com/true/while/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.true.while.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.true.while.LittlePrinceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LittlePrinceGame(), config);
+	}
+}
---
+++ html/src/com/true/while/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.true.while.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.true.while.LittlePrinceGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LittlePrinceGame();
+        }
+}
