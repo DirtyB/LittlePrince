41c71d4f5858fe504585333940e4a089c5c6f8c5
dshevchenkoo <dshevchenkoo@MacBook-Pro-dshevchenkoo.local>
2017-04-22 13:27:41 +0600

    2dscen от Dimassa

--- core/src/com/whiletrue/littleprince/LittlePrinceGame.java
+++ core/src/com/whiletrue/littleprince/LittlePrinceGame.java
@@ -1,13 +1,22 @@
 package com.whiletrue.littleprince;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
-public class LittlePrinceGame extends ApplicationAdapter {
-	SpriteBatch batch;
+
+
+
+public class LittlePrinceGame extends ApplicationAdapter implements ApplicationListener {
+	/*SpriteBatch batch;
 	Texture img;
 	
 	@Override
@@ -29,5 +38,70 @@ public class LittlePrinceGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}*/
+
+
+	public class MyActor extends Actor {
+		Texture texture = new Texture(Gdx.files.internal("../assets/badlogic.jpg"));
+		float actorX = 100, actorY = 100;
+		public boolean started = false;
+
+		public MyActor(){
+			setBounds(actorX,actorY,texture.getWidth(),texture.getHeight());
+			addListener(new InputListener(){
+				public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+					((MyActor)event.getTarget()).started = true;
+					return true;
+				}
+			});
+		}
+
+		@Override
+		public void draw(Batch batch, float alpha){
+			batch.draw(texture,actorX,actorY);
+		}
+
+		@Override
+		public void act(float delta){
+			if(started){
+				actorX+=5;
+			}
+		}
+	}
+
+	private Stage stage;
+
+	@Override
+	public void create() {
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+
+		MyActor myActor = new MyActor();
+		myActor.setTouchable(Touchable.enabled);
+		stage.addActor(myActor);
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
 	}
 }
+
--- desktop/src/com/whiletrue/littleprince/desktop/DesktopLauncher.java
+++ desktop/src/com/whiletrue/littleprince/desktop/DesktopLauncher.java
@@ -4,9 +4,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.whiletrue.littleprince.LittlePrinceGame;
 
-public class DesktopLauncher {
+public class DesktopLauncher  {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new LittlePrinceGame(), config);
-	}
+	    }
+
 }
+
+
+
+
+
